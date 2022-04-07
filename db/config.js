b10fd0b8fa39a8613f948e950669a9d2f75a54a4
Agustin Barrio <agusbarrioeastman@gmail.com>
2022-04-06 23:10:22 -0300

    DB configurada con variables de entorno

--- db/config.js
+++ db/config.js
@@ -1,7 +1,11 @@
+require('dotenv').config({
+  path: `${__dirname}\\..\\.env`,
+});
+console.log(process.env.DB_USER);
 const db = {
-  database: 'personal-budget',
-  username: 'root',
-  password: 'carolita',
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   options: {
     host: 'localhost',
     dialect: 'mysql',
