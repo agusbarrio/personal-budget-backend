4582c0f729a6cce471ba81f1b4c7ff6bcc31d26f
Agustin Barrio <agusbarrioeastman@gmail.com>
2022-04-07 17:09:06 -0300

    correccion sample-data.js

--- db/sample/sample-data.js
+++ db/sample/sample-data.js
@@ -1,8 +1,10 @@
 const fs = require('fs'),
   sampleData = (operationTable) => {
-    fs.readFile(__dirname + '/sample-data.json', 'utf-8', (err, data) => {
-      JSON.parse(data).forEach(async (el) => await operationTable.create(el));
-    });
+    sampleOperations = JSON.parse(
+      fs.readFileSync(__dirname + '/sample-data.json', 'utf8')
+    );
+
+    sampleOperations.forEach(async (el) => await operationTable.create(el));
     console.log('Sample data uploaded successfully');
   };
 
